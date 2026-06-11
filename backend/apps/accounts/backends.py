5567b9636c2d6b1f060009848ac78be316585a19
root <root@srv1346116.hstgr.cloud>
2026-06-11 11:29:13 +0000

    feat: check email before username on login; update label wording

--- backend/apps/accounts/backends.py
+++ backend/apps/accounts/backends.py
@@ -6,11 +6,11 @@ class UsernameOrEmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         User = get_user_model()
         try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
+            user = User.objects.get(email__iexact=username)
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
             try:
-                user = User.objects.get(email__iexact=username)
-            except (User.DoesNotExist, User.MultipleObjectsReturned):
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
                 return None
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
